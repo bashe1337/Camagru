6d9db638ce7429903fbcb427dbfd707c42002367
bashe1337 <55023377+bashe1337@users.noreply.github.com>
2020-08-08 01:08:40 +0300

    start

---
+++ view/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="../css/header.css">
+	<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+	<title>Camagru</title>
+</head>
+<body>
+<header>
+	<div class="header_container">
+		<div class="logo_img"><a class="logo" href="main_page.php">Camagru</div></div>
+		<nav>
+			<img src="../img/icons/heart.svg" alt="h">
+		</nav>
+	</div>	
+</header>
